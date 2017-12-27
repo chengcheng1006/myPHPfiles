82d27367f5c8def501edfc629530636c7bd75f39
chengcheng1006 <chengcheng@smu.edu>
2017-12-26 19:32:24 -0600

    new function

--- phpLearning/function1.php
+++ phpLearning/function1.php
@@ -5,3 +5,27 @@
  * Date: 12/26/17
  * Time: 7:24 PM
  */
+
+    $num1 = 12;
+    $num2 = 12;
+
+    $operator = "+";
+
+    $res = 0;
+    switch ($operator){
+        case "+":
+            $res = $num1 + $num2;
+            break;
+        case "-":
+            $res = $num1 - $num2;
+            break;
+        case "*":
+            $res = $num1 * $num2;
+            break;
+        case "/":
+            $res = $num1 / $num2;
+            break;
+        default:
+            echo "something went wrong!";
+    }
+    echo $num1.$operator.$num2."=".$res;
